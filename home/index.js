e7f37acd6bc4cbfff4d57d9c4de101be728287b5
Minh Anh Nguyen <56661653+minhanh29@users.noreply.github.com>
2020-03-31 20:01:17 -0700

    Add files via upload

---
+++ home/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+/* 2.4.6 - b Current Time - Home Page*/
+//Clock
+setInterval("showClock()", 1000);
+function showClock () {
+    var currentTime = new Date();
+    var eventTime = new Date("April 9, 2020 8:00:00");
+    
+    var daysLeft = (eventTime - currentTime)/(1000*60*60*24);
+    var hrsLeft = (daysLeft - Math.floor(daysLeft))*24;
+    var minsLeft = (hrsLeft - Math.floor(hrsLeft))*60;
+    var secsLeft = (minsLeft - Math.floor(minsLeft))*60;
+
+    var timeLeft = Math.floor(daysLeft) + " days " + Math.floor(hrsLeft) + ":" + Math.floor(minsLeft) + ":" + Math.floor(secsLeft);
+
+    document.getElementById("clock").innerHTML = timeLeft;
+}
