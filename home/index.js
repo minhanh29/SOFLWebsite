3cf7341b587d9f4d7a64528adf593d189ea7291d
Minh Anh Nguyen <56661653+minhanh29@users.noreply.github.com>
2020-04-02 16:17:46 -0700

    Update index.js

--- home/index.js
+++ home/index.js
@@ -17,3 +17,60 @@ function showClock () {
 
     document.getElementById("clock").innerHTML = timeLeft;
 }
+
+
+/*Gallery Slide*/
+var current = 1;  //indicate the current slide's index
+//switch between the 3 slides
+function changeSlide(index) {
+    if (index == 1) {
+        if (current == 3)
+            currentSlide(1);
+        else {
+            current++;
+            currentSlide(current);
+        }
+    }
+
+    if (index == -1) {
+        if (current == 1)
+            currentSlide(3);
+        else {
+            current--;
+            currentSlide(current);
+        }
+    }
+}
+
+//set current slide status
+function currentSlide(index) {
+    var bg = ["url('home/homebG.webp')", "url('home/strawing.webp')", "url('home/swapper.webp')"];  //store 3 image sources
+    var caps = document.getElementsByClassName("caption");   //store 2 captions
+    var container = document.getElementById("gallery-container");   //get the container to change its background
+    var dots = document.getElementsByClassName("dot");
+
+    //change the container background
+    container.style.backgroundImage = bg[index-1];
+
+    //change the dots' backgrounds
+    for (var i = 0; i < dots.length; i++)       //reset backgrounds
+        dots[i].classList.remove("active");
+    dots[index-1].classList.add("active");      //apply active background to the selected dot
+
+    //display the captions 
+    //reset captions' status
+    for (var i = 0; i < caps.length; i++) 
+        caps[i].setAttribute("id", "");
+    switch (index) {
+        case 2:
+            caps[0].setAttribute("id", "show");
+            break;
+        case 3:
+            caps[1].setAttribute("id", "show");
+            break;
+        default:
+            break;
+    }
+
+    current = index;
+}
